5b504787f595bcc05981e27fd24dc350ff7435a9
nabinkhair42 <nabinkhair12@gmail.com>
2025-01-04 23:48:08 +0545

    feat: initialized

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+import { NextRequest } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET })
+  const isAuth = !!token
+  const isAuthPage = req.nextUrl.pathname.startsWith('/signin') || 
+                     req.nextUrl.pathname.startsWith('/signup') ||
+                     req.nextUrl.pathname.startsWith('/verify')
+
+  if (isAuthPage) {
+    if (isAuth) {
+      return NextResponse.redirect(new URL('/dashboard', req.url))
+    }
+    return null
+  }
+
+  if (!isAuth) {
+    let from = req.nextUrl.pathname
+    if (req.nextUrl.search) {
+      from += req.nextUrl.search
+    }
+
+    return NextResponse.redirect(
+      new URL(`/signin?from=${encodeURIComponent(from)}`, req.url)
+    )
+  }
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*', '/signin', '/signup', '/verify']
+}
+
---
+++ src/auth.ts
@@ -0,0 +1,10 @@
+
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import client from "@/lib/db"
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: MongoDBAdapter(client),
+
+  providers: [GitHub],
+})
