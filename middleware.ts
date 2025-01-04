9d73ee384f743064590f7d46c5130952086d39f1
nabinkhair42 <nabinkhair12@gmail.com>
2025-01-05 04:11:12 +0545

    minor: token patch

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,32 @@
-import { NextResponse } from 'next/server'
-import { getToken } from 'next-auth/jwt'
-import { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET })
-  const isAuth = !!token
-  const isAuthPage = req.nextUrl.pathname.startsWith('/signin') || 
-                     req.nextUrl.pathname.startsWith('/signup') ||
-                     req.nextUrl.pathname.startsWith('/verify')
+  const isAuth = req.cookies.has("user-token");
+  const isAuthPage =
+    req.nextUrl.pathname.startsWith("/signin") ||
+    req.nextUrl.pathname.startsWith("/signup") ||
+    req.nextUrl.pathname.startsWith("/verify");
 
   if (isAuthPage) {
     if (isAuth) {
-      return NextResponse.redirect(new URL('/dashboard', req.url))
+      return NextResponse.redirect(new URL("/dashboard", req.url));
     }
-    return null
+    return null;
   }
 
   if (!isAuth) {
-    let from = req.nextUrl.pathname
+    let from = req.nextUrl.pathname;
     if (req.nextUrl.search) {
-      from += req.nextUrl.search
+      from += req.nextUrl.search;
     }
 
     return NextResponse.redirect(
       new URL(`/signin?from=${encodeURIComponent(from)}`, req.url)
-    )
+    );
   }
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/signin', '/signup', '/verify']
-}
-
+  matcher: ["/dashboard/:path*", "/signin", "/signup", "/verify"],
+};
